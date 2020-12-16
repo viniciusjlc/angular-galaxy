b61e54f3715a101b38fb2c1b7e7529b329f10e15
viniciusjlc <viniciusjlc@gmail.com>
2020-12-15 22:24:37 -0300

    - Ajuste regra personagem;

--- src/app/components/cadastrar-personagem/cadastrar-personagem.component.ts
+++ src/app/components/cadastrar-personagem/cadastrar-personagem.component.ts
@@ -4,6 +4,11 @@ import Campanha from '../../models/Campanha';
 import {CampanhaService} from '../../services/campanha/campanha.service';
 import {SessionService} from '../../services/session/session.service';
 import Personagem from '../../models/Personagem';
+import Raca from '../../models/Raca';
+import Classe from '../../models/Classe';
+import TipoCor from '../../models/TipoCor';
+import {Usuario} from '../../models/Usuario';
+import Mestre from '../../models/Mestre';
 
 @Component({
   selector: 'app-cadastrar-personagem',
@@ -11,10 +16,18 @@ import Personagem from '../../models/Personagem';
   styleUrls: ['./cadastrar-personagem.component.css']
 })
 export class CadastrarPersonagemComponent implements OnInit {
-  items: MenuItem[];
+  itens: MenuItem[];
   abaAtual: number = 0;
   campanhas: Campanha[];
-  personagem: Personagem;
+  personagem: Personagem = new Personagem(null,
+    new Raca(null, null, null, null, null, null, null, null, null),
+    new Classe(null, null, null),
+    null, null, null, null,
+    new TipoCor(null, null, null),
+    new TipoCor(null, null, null),
+    new TipoCor(null, null, null),
+    new SessionService().userSession.id,
+    new Campanha(null, null, new Mestre(null, null)));
 
   constructor(private campanhaServive: CampanhaService,
               private confirmationService: ConfirmationService) {
@@ -25,7 +38,8 @@ export class CadastrarPersonagemComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.items = [{
+    this.iniciarListaCampanhaPorJogadorAtual();
+    this.itens = [{
       label: 'Campanha'
     },
       {
@@ -38,15 +52,22 @@ export class CadastrarPersonagemComponent implements OnInit {
         label: 'História'
       }
     ];
-    this.iniciarListaCampanhaPorJogadorAtual();
+    console.log(this.abaAtual);
   }
 
-  nextPage(): void{
+  nextPage(): void {
     this.abaAtual++;
   }
 
-  prevPage(): void{
+  prevPage(): void {
     this.abaAtual--;
   }
 
+  retornarCor(idCampanha): string {
+    return this.personagem.campanha.id === idCampanha ? '#f5cf00' : '#a47e19';
+  }
+
+  clicar(campanhaSelecionada): void {
+    console.log(campanhaSelecionada);
+  }
 }
