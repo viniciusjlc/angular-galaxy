0aa7dadce4803868e23bfa425a247f3b5045f986
viniciusjlc <viniciusjlc@gmail.com>
2021-02-11 20:39:48 -0300

    - Ajustes para acessar dados pela nova visualização;

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {Dialog} from 'primeng/dialog';
+import {Usuario} from '../../models/Usuario';
+import {SessionService} from '../../services/session/session.service';
+import {UsuarioService} from '../../services/usuario/usuario.service';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,10 +11,87 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
+  isLogado: boolean = false;
 
-  constructor() { }
+  usuario: Usuario = new Usuario('teste@teste.com', 'Vinicius', 'teste');
+  usuarioCadastro: Usuario = new Usuario('vaches@teste.com', 'Vaches', 'teste');
+
+  showLogin: boolean = false;
+  showCadastro: boolean;
+
+  confirmacaoSenha: string = '';
+
+  mensagemErroLogin: string;
+  mensagemErroCadastro: string;
+
+  constructor(private usuarioService: UsuarioService,
+              private messageService: MessageService) {
+  }
 
   ngOnInit(): void {
   }
 
+  showDialog(dialog: Dialog): void {
+    if (window.innerWidth < 993) {
+      console.log();
+      dialog.maximize();
+    }
+  }
+
+  retornarTitulo(): string {
+    return this.showCadastro ? 'Cadastrar-se' : 'Logar';
+  }
+
+  async logar(): Promise<void> {
+    if (this.usuario.email === '' || this.usuario.senha === '') {
+      this.mensagemErroLogin = 'Email e Senha obrigatórios!';
+    } else {
+      this.isLogado = await this.usuarioService.autenticar(this.usuario);
+      if (this.isLogado) {
+        this.showLogin = false;
+        this.usuario = await this.usuarioService.consultarPorEmail({
+          email: this.usuario.email,
+          senha: this.usuario.senha
+        });
+        SessionService.instace.gravarUsuario(this.usuario);
+        this.messageService.add({
+          key: 'tc',
+          severity: 'success',
+          summary: 'Sucesso',
+          detail: 'Login realizado com sucesso'
+        });
+      } else {
+        this.mensagemErroLogin = 'Email ou Senha incorretos!';
+      }
+    }
+  }
+
+  async cadastrarUsuario(): Promise<void> {
+    console.log('teste');
+    if (
+      this.usuarioCadastro.email === ''
+      || this.usuarioCadastro.senha === ''
+      || this.usuarioCadastro.nome === ''
+      || this.confirmacaoSenha === '') {
+      this.mensagemErroCadastro = 'Todos os campos são obrigatórios!';
+    } else if (this.usuarioCadastro.senha !== this.confirmacaoSenha) {
+      this.mensagemErroCadastro = 'Senha e confimação devem ser iguais!';
+    } else {
+      const retorno: boolean = await this.usuarioService.cadastrar(this.usuarioCadastro);
+      if (retorno) {
+        this.showLogin = false;
+        this.showCadastro = false;
+        this.isLogado = true;
+        this.usuario = this.usuarioCadastro;
+        this.messageService.add({
+          key: 'tc',
+          severity: 'success',
+          summary: 'Sucesso',
+          detail: 'Cadastro realizado com sucesso'
+        });
+      } else {
+        this.mensagemErroCadastro = 'Erro ao realizar o cadastro! Entre em contato com o administrador do sistema';
+      }
+    }
+  }
 }
